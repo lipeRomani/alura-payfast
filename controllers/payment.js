b31afaf2c37ec92ae47d3031a39d7ac9852f75dd
Felipe Augusto Romani <felipearomani@gmail.com>
2017-10-01 22:28:25 -0300

    feature: adicionados metodos novos
    Adicionado rotas para:
    	confirmacao de pagmento,
    	cancelamento de pagamento

--- controllers/payment.js
+++ controllers/payment.js
@@ -1,14 +1,39 @@
+const {check, validationResult} = require('express-validator/check');
+const { matchedData, sanitize } = require('express-validator/filter');
+
+const currencys = ['BRL', 'USD']
+const validationPaymentCreate = [
+        check('value')
+            .isDecimal()
+            .withMessage('Value deve ser um decimal.'),
+        check('description')
+            .isLength({min : 5, max: 150})
+            .withMessage('Descrição deve possuir no mínimo 5 caracteres'),
+        check('currency')
+            .isIn(currencys)
+            .withMessage('Moeda precisa ser BRL (real) ou UDS (dolar)')
+    ];
+
 module.exports = (app) => {
 
     app.get('/payment', (req, res) => {
         res.send('Hello world')
     });
 
-    app.post('/payment/pay', (req, res) => {
+    app.get('/payment/:id', (req, res) => {
+        res.send('Hello world')
+    });
+
+    app.post('/payment/pay', validationPaymentCreate, (req, res) => {
         const payment = req.body;
         payment.status = 'Created';
         payment.date = new Date;
 
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.mapped() });
+        }
+        
         const conn = app.persistence.connectionFactory;
         const dao  = new app.persistence.PaymentDao(conn);
         
@@ -17,11 +42,67 @@ module.exports = (app) => {
                 return dao.findById(result.insertId);
             })
             .then(payment => {
-                res.json(payment);
+                const hateoas = {
+                    payment,
+                    links : {
+                        delete : {
+                            path : `/payment/${payment.id}`,
+                            method : 'DELETE' 
+                        },
+                        confirm : {
+                            path : `/payment/${payment.id}`,
+                            method : 'PUT'
+                        },
+                        detail : {
+                            path : `/payment/${payment.id}`,
+                            method : 'GET' 
+                        }
+                    }
+                }
+                res.status(201).json(hateoas);
             })
             .catch(err => {
                 console.log(`Error Occurred: ${err.message}`)
-                res.json({error : err.message});
+                res.status(500).json({error : err.message});
+            });
+    });
+
+    app.put('/payment/:id', (req, res) => {
+        
+        const paymentId = req.params.id;
+        const conn = app.persistence.connectionFactory;
+        const dao  = new app.persistence.PaymentDao(conn);
+        
+        dao.findById(paymentId)
+            .then(payment => {
+                if (!payment) res.status(404).json({message : "Pagamento não encontrado"});
+                return dao.confirmPayment(payment);
+            })
+            .then(payment => {
+                res.status(200).json(payment)
+            })
+            .catch(err => {
+                res.status(500).json({error : err.message})
+            });
+    });
+
+    app.delete('/payment/:id', (req, res) => {
+        
+        const paymentId = req.params.id;
+        const conn = app.persistence.connectionFactory;
+        const dao  = new app.persistence.PaymentDao(conn);
+        
+        dao.findById(paymentId)
+            .then(payment => {
+                if (!payment) res.status(404).json({message : "Pagamento não encontrado"});
+                return dao.deletePayment(payment);
+            })
+            .then(payment => {
+                res.status(204).json({message : "Pagamento cancelado com sucesso"})
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({error : err.message})
             });
     });
 }
--- persistence/PaymentDao.js
+++ persistence/PaymentDao.js
@@ -5,11 +5,11 @@ class PaymentDao {
         this._con = con;
     }
 
-    insert({value, description, currency, status, date}) {
+    insert(payment) {
         return new Promise((resolve, reject) => {
           this._con.query(
-            "INSERT INTO payments (description, value, currency, status, date) VALUES (?)",
-            [[description, value, currency, status, date]],
+            "INSERT INTO payments SET ?",
+            payment,
             (err, result) => {
                 if (err) reject(err);
                 resolve(result);
@@ -21,9 +21,35 @@ class PaymentDao {
         return new Promise((resolve, reject) => {
             this._con.query("SELECT * FROM payments WHERE id = ?", [id], (err, result) => {
                 if (err) reject(err);
-                if (result.length === 0) resolve(result);
+                if (result.length === 0) resolve(null);
                 resolve(result[0]);
             });  
+        });
+    }
+
+    confirmPayment(payment) {
+        payment.status = 'Confirmed';
+        return new Promise((resolve, reject) => {
+            this._con.query(
+                'UPDATE payments SET status = ? WHERE id = ?',
+                [payment.status, payment.id], 
+                (err, result) => {
+                    if (err) reject(err);
+                    resolve(payment); 
+                })
+        });
+    }
+
+    deletePayment(payment) {
+        payment.status = 'Cancelled';
+        return new Promise((resolve, reject) => {
+            this._con.query(
+                'UPDATE payments SET status = ? WHERE id = ?',
+                [payment.status, payment.id], 
+                (err, result) => {
+                    if (err) reject(err);
+                    resolve(payment); 
+                })
         });
     }
 
